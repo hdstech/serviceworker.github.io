1cd7e3a381f573dac56df4704a451fa28e72449f
HDS Tech <hdstech@HDSs-MBP.lan1>
2020-01-29 23:43:36 -0500

    Changes file location

---
+++ js/sw_cached_pages.js
@@ -0,0 +1,43 @@
+const cacheName = "v1";
+
+const cacheAssets = ["../index.html", "../about.html", "../css/style.css", "./main.js"];
+
+//Call install event
+self.addEventListener("install", e => {
+  console.log("Service worker: installed");
+
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log("Service worker: Caching files");
+        cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+//Call activate event
+self.addEventListener("activate", e => {
+  console.log("Service worker: activated");
+
+  //Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log("Service worker: clearing old cache");
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+//Call fetch event
+self.addEventListener("fetch", e => {
+  console.log("Service worker: fetching...");
+  e.respondWith(fetch(e.request).catch(e => caches.match(e.request)));
+});
--- js/sw_cached_site.js
+++ js/sw_cached_site.js
@@ -1,20 +1,8 @@
-const cacheName = "v1";
-
-const cacheAssets = ["/index.html", "/about.html", "/css/style.css", "/js/main.js"];
+const cacheName = "v2";
 
 //Call install event
 self.addEventListener("install", e => {
   console.log("Service worker: installed");
-
-  e.waitUntil(
-    caches
-      .open(cacheName)
-      .then(cache => {
-        console.log("Service worker: Caching files");
-        cache.addAll(cacheAssets);
-      })
-      .then(() => self.skipWaiting())
-  );
 });
 
 //Call activate event
@@ -39,5 +27,16 @@ self.addEventListener("activate", e => {
 //Call fetch event
 self.addEventListener("fetch", e => {
   console.log("Service worker: fetching...");
-  e.respondWith(fetch(e.request).catch(e => caches.match(e.request)));
+  e.respondWith(e.request)
+    .then(res => {
+      //Make clone of response
+      const resClone = res.clone();
+      //Open cache
+      caches.open(cacheName).then(cache => {
+        //Add response to cache
+        cache.put(e.request, resClone);
+      });
+      return res;
+    })
+    .catch(() => caches.match(e.request).then(res => res));
 });
